755705a24532ea5cf4a4d5a34abe9d3be9589297
Nathan Heffley <nathan@nathanheffley.com>
2022-12-09 22:04:11 -0500

    2019 Day 7 Part 1

--- 2015/09/main.go
+++ 2015/09/main.go
@@ -36,7 +36,7 @@ func main() {
 		connections[cityB][cityA] = distance
 	}
 
-	routes := permutations(cities)
+	routes := helpers.Permutate(cities)
 
 	shortestRoute := 9999999
 	shortestRouteName := ""
@@ -63,32 +63,4 @@ func main() {
 	fmt.Println(shortestRouteName)
 	fmt.Println("Longest route:")
 	fmt.Print(longestRouteName)
-}
-
-func permutations(arr []string) [][]string {
-	var helper func([]string, int)
-	res := [][]string{}
-
-	helper = func(arr []string, n int) {
-		if n == 1 {
-			tmp := make([]string, len(arr))
-			copy(tmp, arr)
-			res = append(res, tmp)
-		} else {
-			for i := 0; i < n; i++ {
-				helper(arr, n-1)
-				if n%2 == 1 {
-					tmp := arr[i]
-					arr[i] = arr[n-1]
-					arr[n-1] = tmp
-				} else {
-					tmp := arr[0]
-					arr[0] = arr[n-1]
-					arr[n-1] = tmp
-				}
-			}
-		}
-	}
-	helper(arr, len(arr))
-	return res
 }
---
+++ 2019/07/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nathanheffley/advent-of-code/helpers"
+	"github.com/nathanheffley/advent-of-code/intcode"
+)
+
+func main() {
+	program := "3,8,1001,8,10,8,105,1,0,0,21,38,59,84,93,110,191,272,353,434,99999,3,9,101,5,9,9,1002,9,5,9,101,5,9,9,4,9,99,3,9,1001,9,3,9,1002,9,2,9,101,4,9,9,1002,9,4,9,4,9,99,3,9,102,5,9,9,1001,9,4,9,1002,9,2,9,1001,9,5,9,102,4,9,9,4,9,99,3,9,1002,9,2,9,4,9,99,3,9,1002,9,5,9,101,4,9,9,102,2,9,9,4,9,99,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,99"
+
+	maxSignal := 0
+
+	for _, sequence := range helpers.Permutate[int]([]int{0, 1, 2, 3, 4}) {
+		// A
+		input := make(chan int)
+		output := make(chan int)
+		go intcode.Execute(program, input, output)
+		input <- sequence[0]
+		input <- 0
+		result := <-output
+
+		// B
+		input = make(chan int)
+		output = make(chan int)
+		go intcode.Execute(program, input, output)
+		input <- sequence[1]
+		input <- result
+		result = <-output
+
+		// C
+		input = make(chan int)
+		output = make(chan int)
+		go intcode.Execute(program, input, output)
+		input <- sequence[2]
+		input <- result
+		result = <-output
+
+		// D
+		input = make(chan int)
+		output = make(chan int)
+		go intcode.Execute(program, input, output)
+		input <- sequence[3]
+		input <- result
+		result = <-output
+
+		// E
+		input = make(chan int)
+		output = make(chan int)
+		go intcode.Execute(program, input, output)
+		input <- sequence[4]
+		input <- result
+		result = <-output
+
+		if result > maxSignal {
+			maxSignal = result
+		}
+	}
+
+	fmt.Println(maxSignal)
+	fmt.Println("(official answer for program: 225056)")
+}
